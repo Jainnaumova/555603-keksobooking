9cb018bc8132f6a7cad0e360f45e76021ad016ec
jainnaumova <jainnaumova@gmail.com>
2018-04-09 00:11:51 -0400

    Добавила рандомное создание метки и заполнение блока+ошибки от Тревиса

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 
 // Массив данных для предложения
 var titleOffer = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var typeOffer = ['palace', 'flat', 'house', 'bungalo'];
+var typeOffer = ['Дворец', 'Квартира', 'Дом', 'Лачуга'];
 var checkinOffer = ['12:00', '13:00', '14:00'];
 var checkoutOffer = ['12:00', '13:00', '14:00'];
 var featureOffer = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -10,7 +10,7 @@ var photosOffer = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http:/
 
 // Функция для получения рандомного числа из двух крайних
 function getRandomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Функция для получения рандомного элемента массива
@@ -22,20 +22,22 @@ function getRandomElementArray(ArrayName) {
 // Функция для перемешивания элементов массива
 function sortRandomElementArray(ArrayName) {
   function mixElementArray() {
-    return Math.random - 0.5
-  };
+    return Math.random - 0.5;
+  }
   return ArrayName.sort(mixElementArray)
 }
 
 // Массив предложения
 var dataOffer = {
   author: {
-    avatar: 'img/avatars/user0' + getRandomInteger(1,8) + '.png'
+    avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png'
   },
 
   offer: {
     title: getRandomElementArray(titleOffer),
-    address: "dataOffer.location.x + ', ' + dataOffer.location.y",
+    address: function() {
+      return dataOffer.location.x + ', ' + dataOffer.location.y;
+    },
     price: getRandomInteger(1000, 1000000),
     type: getRandomElementArray(typeOffer),
     rooms: getRandomInteger(1, 5),
@@ -53,37 +55,39 @@ var dataOffer = {
     x: getRandomInteger(300, 900),
     y: getRandomInteger(150, 500)
   }
-}
+};
 
 // Удаляем класс map__faded
 var activeMap = document.querySelector('.map');
 activeMap.classList.remove('map--faded');
 
-// Функция создания метки на карте
-function createPin() {
+// Функция рандомного создания метки на карте
+
+function createPin(entryArray) {
   var pinTemplate = document.querySelector('#card-template').content.querySelector('.map__pin');
   var pinImg = pinTemplate.querySelector('img');
-  pinTemplate.style = 'left: 300px; top: 500px;'
-  pinImg.scr = 'img/avatars/user01';
-  pinImg.alt = 'New Offer';
+  var pinCoordinate = "'left: ' + entryArray.location.x + 'px' + 'top: ' + entryArray.location.y + 'px'";
+  pinTemplate.style = pinCoordinate;
+  pinImg.scr = entryArray.author.avatar;
+  pinImg.alt = entryArray.offer.title;
   return pinTemplate;
 }
-activeMap.querySelector('.map__pins').appendChild(createPin());
+activeMap.querySelector('.map__pins').appendChild(createPin(dataOffer));
 
-// Функция заполнения блока DOM-элементами
-function createMapCard() {
+// Функция рандомного заполнения блока DOM-элементами
+function createMapCard(entryArray) {
   var mapCardTemplate = document.querySelector('#card-template').content.querySelector('.map__card');
-  mapCardTemplate.querySelector('.popup__title').textContent = 'Горящее предложение!';
-  mapCardTemplate.querySelector('.popup__text--address').textContent = 'Адрес квартиры';
-  mapCardTemplate.querySelector('.popup__text--price').textContent = '2300 ' + 'Р/ночь';
-  mapCardTemplate.querySelector('.popup__type').textContent = 'Квартира';
-  mapCardTemplate.querySelector('.popup__text--capacity').textContent = '1 комната для 2 гостей'
-  mapCardTemplate.querySelector('.popup__text--time').textContent = 'Заезд после 12:00 , выезд до 14:00';
+  mapCardTemplate.querySelector('.popup__title').textContent = entryArray.offer.title;
+  mapCardTemplate.querySelector('.popup__text--address').textContent = entryArray.offer.address();
+  mapCardTemplate.querySelector('.popup__text--price').textContent = entryArray.offer.price + ' Р/ночь';
+  mapCardTemplate.querySelector('.popup__type').textContent = entryArray.offer.type;
+  mapCardTemplate.querySelector('.popup__text--capacity').textContent = entryArray.offer.rooms + ' комната для ' + entryArray.offer.guests + ' гостей';
+  mapCardTemplate.querySelector('.popup__text--time').textContent = 'Заезд после ' + entryArray.offer.checkin + ' ,' + ' выезд до ' + entryArray.offer.checkout;
   mapCardTemplate.querySelector('.popup__features').classList.add('popup__feature--wifi');
-  mapCardTemplate.querySelector('.popup__description').textContent = 'Описание горящего предложения';
-  mapCardTemplate.querySelector('.popup__photos img').src = 'http://o0.github.io/assets/images/tokyo/hotel1.jpg';
+  mapCardTemplate.querySelector('.popup__description').textContent = entryArray.offer.description;
+  mapCardTemplate.querySelector('.popup__photos img').src = 'http://o0.github.io/assets/images/tokyo/hotel1.jpg'
   return mapCardTemplate;
 };
 // Замените src у аватарки пользователя — изображения, которое записано в .popup__avatar — на значения поля author.avatar отрисовываемого объекта.???
 
-activeMap.appendChild(createMapCard());
+activeMap.appendChild(createMapCard(dataOffer));
