68195c022af66b0114b2b6cbc22f2da946d5a91e
jainnaumova <jainnaumova@gmail.com>
2018-04-20 01:00:50 -0400

    Добавляем подвижности

--- js/map.js
+++ js/map.js
@@ -347,3 +347,56 @@ function resetInputPrice() {
   inputHousePrice.placeholder = '1000';
   inputHousePrice.min = '0';
 }
+// Добавляем подвижности
+var BORDER_TOP_MAIN_PIN_FIELD = 150;
+var BORDER_RIGHT_MAIN_PIN_FIELD = 1150;
+var BORDER_BOTTOM_MAIN_PIN_FEILD = 500;
+var BORDER_LEFT_MAIN_PIN_FIELD = 0;
+
+var mainPinHandle = mainPin.querySelector('img');
+mainPinHandle.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  function mouseMoveHandler(moveEvt) {
+    moveEvt.preventDefault();
+
+    // Функция ограничитель движения пина
+    function restrictMainPinFieldMoving(pinCoords, min, max) {
+      return Math.max(min, Math.min(pinCoords, max));
+    }
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    var mainPinLeft = mainPin.offsetLeft - shift.x;
+    var mainPinTop = mainPin.offsetTop - shift.y;
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mainPinLeft = restrictMainPinFieldMoving(mainPinLeft, BORDER_LEFT_MAIN_PIN_FIELD, BORDER_RIGHT_MAIN_PIN_FIELD);
+    mainPinTop = restrictMainPinFieldMoving(mainPinTop, BORDER_TOP_MAIN_PIN_FIELD, BORDER_BOTTOM_MAIN_PIN_FEILD);
+    mainPin.style.top = (mainPinTop) + 'px';
+    mainPin.style.left = (mainPinLeft) + 'px';
+    addressFieldset.value = (mainPinLeft) + ' ,' + (mainPinTop);
+  }
+
+  function mouseUpHandler(upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  }
+
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
+});
